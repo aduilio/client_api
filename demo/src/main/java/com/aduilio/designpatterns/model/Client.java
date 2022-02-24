6372a477ee759eac2d40d6162be1b7a1b3f26eb6
Aduilio Silva <developer.aduilio@gmail.com>
2022-02-24 16:19:26 -0300

    Create model classes

---
+++ demo/src/main/java/com/aduilio/designpatterns/model/Client.java
@@ -0,0 +1,21 @@
+package com.aduilio.designpatterns.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	@ManyToOne
+	private Address address;
+}
